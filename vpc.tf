7cee1a5e1d39a12d2a544f32ae7c29afc3903472
vikasvarmadunna <81794500+vikasvarmadunna@users.noreply.github.com>
2023-01-29 11:09:58 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "pavan" {
+resource "aws_vpc" "main" {
   cidr_block       = "10.1.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "pavan"
+    Name = "main"
   }
 }
